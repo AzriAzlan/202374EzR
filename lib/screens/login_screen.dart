343e526c8579b3fd2951b79112d3ee0f34cea7d7
verseRe <83128485+verseRe@users.noreply.github.com>
2021-06-23 23:48:17 +0800

    Update login_screen.dart
    Add UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,8 +2,11 @@ import 'package:ezrestaurantapp/screens/register_screen.dart';
 import 'package:ezrestaurantapp/services/auth.dart';
 import 'package:ezrestaurantapp/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
+  bool usernameEmpty = false;
+  bool passwordEmpty = false;
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -11,92 +14,159 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
 
   final Authenticate _auth = Authenticate();
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
 
   @override
   void dispose() {
     // Clean up the controller when the widget is disposed.
-    usernameController.dispose();
-    passwordController.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
+
+  void onLogin() async {
+    setState(() {
+      _usernameController.text.isEmpty ? widget.usernameEmpty = true : widget.usernameEmpty = false;
+      _passwordController.text.isEmpty ? widget.passwordEmpty = true : widget.passwordEmpty = false;
+    });
+    if(_usernameController.text.isEmpty || _passwordController.text.isEmpty) {
+      return;
+    }
+    else {
+      dynamic result = await _auth.signInEmail(_usernameController.text,_passwordController.text);
+
+      if(result==null) {
+        print("error signin");
+        return;
+      }
+      else {
+        print("signedin");
+        print(result);
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
 
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
 
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/bg.png"),
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
 
-        body: Center(
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("WELCOME TO EZRESTAURANT"),
-
-              SizedBox(
-                width: 300,
-                child: TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(labelText: 'Username'),
-                ),
-              ),
-
-              SizedBox(
-                width: 300,
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                ),
-              ),
-
-              SizedBox(height: 20),
-
-              Builder(
-                builder: (context) =>
-                ElevatedButton(
-                    onPressed: () async{
-
-                      dynamic result = await _auth.signInEmail(usernameController.text,passwordController.text);
-
-                      if(result==null) {
-                        print("error signin");
-                      } else {
-                        print("signedin");
-                        print(result);
-
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeScreen()),
-                        );
-                      }
-
-                  //sign in
-                      },
-
-                    child: Text("Sign in")),
-              ),
-
-              SizedBox(height: 20),
-
-              Builder(
-                builder: (context) => GestureDetector(
-                    child: Text("Dont have an account ? Sign up here"),
-                  onTap: ()=> Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegisterScreen()),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 150.0),
+                  child: SizedBox(
+                    width: 200,
+                    child: Image(
+                      image: AssetImage('assets/logo.png'),
+                    ),
                   ),
                 ),
-              )
 
-            ],
+                Text("WELCOME TO EZRESTAURANT"),
+
+                SizedBox(height: 30),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        helperText: widget.usernameEmpty ? "Required":"",
+                        helperStyle: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: SizedBox(
+                    width: 300,
+                    child: TextField(
+                      obscureText: true,
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        helperText: widget.passwordEmpty ? "Required":"",
+                        helperStyle: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                SizedBox(
+                  width: 200,
+                  child: TextButton(
+                    onPressed: onLogin,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.white),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40.0),
+                                side: BorderSide(color: Colors.grey)
+                            )
+                        )
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Builder(
+                  builder: (context) => GestureDetector(
+                      child: Text("Dont have an account ? Sign up here"),
+                    onTap: ()=> Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterScreen()),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+
           ),
-
         ),
       ),
     );
